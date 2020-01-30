d51aeee3c3832ba405f5473e4e1cadf06f8dae92
vividswan <vividswan@naver.com>
2020-01-31 01:52:29 +0900

    Feat Icons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <v-app>
     <v-app-bar app color="white">
-      <span class="display-1">Simple Board</span>
+      <v-icon left>mdi-post</v-icon>
+      <span class="display-1"> Simple </span>
+      <span class="display-1 font-weight-black">Board</span>
       <v-spacer></v-spacer>
-      <span class="subtitle-1">NEMV Stack</span>
+      <v-icon left>mdi-radar</v-icon>
+      <span class="subtitle-1 font-italic font-weight-medium">NEMV Stack </span>
     </v-app-bar>
     <br />
     <v-content>

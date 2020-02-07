78728eafccb7c9f307d06df21f3758eac78bdc61
vividswan <vividswan@naver.com>
2020-02-07 14:20:41 +0900

    Update PostList.vue

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -13,6 +13,7 @@
         :page.sync="page"
         :items-per-page="5"
         hide-default-footer
+        :mobile-breakpoint="NaN"
         class="elevation-1 blue-grey lighten-5"
         @page-count="pageCount = $event"
       >

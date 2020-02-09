282e6f01703b0a7865be8348efc746aa799af308
vividswan <vividswan@naver.com>
2020-02-10 01:57:20 +0900

    Add axios to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.prototype.$http = axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");

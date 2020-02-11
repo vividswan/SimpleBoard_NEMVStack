f79b5f75c3c5fd2048bb046a79459a096c3d703b
vividswan <vividswan@naver.com>
2020-02-11 15:34:06 +0900

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   devServer: {
+    // 1.api 요청이 있을때 어디에서 처리할지를 설정
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true
+        target: "http://localhost:3000/api",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
       }
     }
   },
-  transpileDependencies: ["vuetify"]
+  outputDir: "../backend/public" // 2.배포 파일의 위치를 지정
 };

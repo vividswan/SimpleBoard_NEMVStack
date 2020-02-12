a921cf1aebdad67b74cf3a561e63ab969c0e41be
vividswan <vividswan@naver.com>
2020-02-12 14:55:35 +0900

    Update post.js

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,10 +1,10 @@
 var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
 
-var postSchema = new Schema({
+var postSchema = new mongoose.Schema({
   title: String,
   author: String,
   created_date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("post", postSchema);
+// create Model
+module.exports = mongoose.model("Post", postSchema);

d6054bf788a212496602a3e72ed8e72387d16c32
vividswan <vividswan@naver.com>
2020-02-14 21:04:24 +0900

    Update PostList.vue

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -81,9 +81,7 @@
             <v-btn color="blue darken-1" text @click="dialog = false"
               >Close</v-btn
             >
-            <v-btn color="blue darken-1" text @click="dialog = false"
-              >Save</v-btn
-            >
+            <v-btn color="blue darken-1" text @click="writePost()">Save</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -103,6 +101,9 @@ export default {
     makePost() {},
     clickWrite() {
       this.dialog = true;
+    },
+    writePost() {
+      this.dialog = false;
     }
   },
   data() {

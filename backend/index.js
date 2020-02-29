a3d3c58f8d94a424074d2761e3af8a09e10b1a04
vividswan <vividswan@naver.com>
2020-02-29 16:04:50 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-require("dotenv").config();
+require("dotenv").config(); // 환경변수 사용을 위한 라이브러리
 
 mongoose
   .connect(process.env.DB_URL, {
@@ -12,7 +12,7 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));

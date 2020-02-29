8d27b632f6557ddef84a9e784cc409361f452f4a
vividswan <vividswan@naver.com>
2020-02-29 19:35:47 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const { Post } = require("./models/Post");
 const bodyParser = require("body-parser"); //클라이언트에게 req.body를 받기 위한 dependency
 const postApi = require("./routes/api/index");
 const cors = require("cors");
@@ -28,6 +27,7 @@ app.use("/api", postApi);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+/*
 app.post("/add", (req, res) => {
   // 게시글을 만들 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
@@ -41,6 +41,7 @@ app.post("/add", (req, res) => {
     });
   });
 });
+*/
 
 app.listen(process.env.PORT, () =>
   console.log(`app listening on port ${process.env.PORT}!`)

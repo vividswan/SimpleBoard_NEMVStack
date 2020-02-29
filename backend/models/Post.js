35895091eb8703e24e6ba87588d4b51fc8b7a1e3
vividswan <vividswan@naver.com>
2020-02-29 16:04:46 +0900

    Create Post.js

---
+++ backend/models/Post.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  title: { type: String, default: "", maxlength: 50 },
+  author: { type: String, default: "이름없음" },
+  created_date: { type: Date, default: Date.now() }
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = { Post };

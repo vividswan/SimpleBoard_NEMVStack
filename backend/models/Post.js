ce1012dbb8a8c5469cf2b97526e03b7f74291a1d
vividswan <vividswan@naver.com>
2020-03-07 07:12:45 +0900

    Feat idNumber function(mongoose-auto-increment)

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+const autoIncrement = require("mongoose-auto-increment");
 
 const postSchema = mongoose.Schema({
   title: { type: String, default: "", maxlength: 50 },
   content: { type: String, default: "" },
   author: { type: String, default: "이름없음" },
-  created_date: { type: Date, default: Date.now() }
+  created_date: { type: Date, default: Date.now }
 });
 
-const Post = mongoose.model("Post", postSchema);
+autoIncrement.initialize(mongoose.connection);
+postSchema.plugin(autoIncrement.plugin, "post");
 
+const Post = mongoose.model("Post", postSchema);
 module.exports = Post;

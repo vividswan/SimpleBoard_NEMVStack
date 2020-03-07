89b643eddf8aa5c906543d4984732df046d9a32d
vividswan <vividswan@naver.com>
2020-03-07 09:13:35 +0900

    Feat router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import axios from "axios";
+import router from "./router";
 
 Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount("#app");

05be2618c056caa0eaed18c95615a4ea5f9539a4
vividswan <vividswan@naver.com>
2020-03-11 22:17:08 +0900

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const autoIncrement = require("mongoose-auto-increment");
 
 const postSchema = mongoose.Schema({
-  title: { type: String, default: "", maxlength: 50 },
-  content: { type: String, default: "" },
-  author: { type: String, default: "이름없음" },
+  title: { type: String, default: "NoTitle", maxlength: 50 },
+  content: { type: String, default: "NoContent" },
+  author: { type: String, default: "NoAuthor" },
   created_date: { type: Date, default: Date.now }
 });
 
